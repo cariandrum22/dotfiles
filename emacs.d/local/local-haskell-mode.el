9eedccc12d67a5e7567435a5b4021ba67ad40e24
Takafumi Asano <cariandrum22@gmail.com>
2014-01-30 11:23:13 +0900

    Modify few emacs plugins and gitignore
    - Install slime
    - Update scala-mode to scala-mode2
    - Modify haskell-mode setting
    - Modify gitignore(for IntelliJ, Typesafe Activator, SBT)

--- emacs.d/local/local-haskell-mode.el
+++ emacs.d/local/local-haskell-mode.el
@@ -7,4 +7,4 @@
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
-(setq haskell-program-name "/usr/bin/ghci")
+(setq haskell-program-name "/usr/local/bin/ghci")
--- emacs.d/local/local-scala-mode.el
+++
@@ -1,8 +0,0 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; load scala-mode
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(provide 'local-scala-mode)
-(require 'scala-mode-auto)
-(add-hook 'scala-mode-hook
-          '(lambda ()
-             (yas/minor-mode-on)))
