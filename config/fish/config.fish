3cbb9b7a51d926cc4a7c7a392d6765bc17272b9a
Takafumi Asano <cariandrum22@gmail.com>
2017-05-23 17:32:53 +0900

    Add fish and fisherman config

---
+++ config/fish/config.fish
@@ -0,0 +1,86 @@
+# initialize
+thunnus.init
+
+# environment variables
+set -x LC_ALL en_US.UTF-8
+set -x LANG en_US.UTF-8
+set -x PATH "/usr/local/sbin" $PATH
+set -x EDITOR (which atom)
+set -x VISUAL (which atom)
+
+# export default aws credentials
+aws_credential_selector codetakt:cariandrum22 tf
+
+# AWS
+set -x AWS_DEFAULT_REGION "ap-northeast-1"
+
+# Google Computing Platform
+## The next line updates PATH for the Google Cloud SDK.
+if [ -f $HOME/Applications/Google/google-cloud-sdk/path.zsh.inc ]
+  bass source "$HOME/Applications/Google/google-cloud-sdk/path.bash.inc"
+end
+
+## The next line enables shell command completion for gcloud.
+if [ -f $HOME/Applications/Google/google-cloud-sdk/completion.zsh.inc ]
+  bass source "$HOME/Applications/Google/google-cloud-sdk/completion.bash.inc"
+end
+
+# rbenv
+rbenv init - | source
+
+# pyenv
+pyenv init - | source
+pyenv virtualenv-init - | source
+
+# plenv
+plenv init - | source
+
+# nodebrew
+set_path "$HOME/.nodebrew/current/bin"
+set -x NODE_PATH "$HOME/.nodebrew/current/lib/node_modules/"
+
+# Stack
+set_path "$HOME/.local/bin"
+
+# rustup
+set_path "$HOME/.cargo/bin"
+
+# go
+set -x GOPATH "$HOME/.go"
+set_path "$GOPATH/bin"
+
+# java
+set -x JAVA_HOME (/usr/libexec/java_home -v 1.8)
+
+# python
+set -x PYTHONSTARTUP "$HOME/.pythonstartup"
+
+# Terraform
+#set_path "$HOME/Applications/HashiCorp/Terraform/current/bin"
+
+# Ansible
+set -x ANSIBLE_COW_SELECTION "random"
+
+# TeX
+set_path '/Library/TeX/Distributions/.DefaultTeX/Contents/Programs/texbin'
+
+# Nand2tetris Software Suite
+set_path "$HOME/Applications/nand2tetris/tools"
+
+# alias
+
+## Generic
+alias where="command -v"
+alias j="jobs -l"
+alias ll="ls -lh"
+alias la="ls -lha"
+
+## Haskell
+#. $HOME/.nix-profile/etc/profile.d/nix.sh
+alias ghc="stack ghc"
+alias ghci="stack ghci"
+alias runghc="stack runghc"
+alias runhaskell="stack runghc"
+
+# OPAM configuration
+source $HOME/.opam/opam-init/init.fish > /dev/null 2> /dev/null or true
