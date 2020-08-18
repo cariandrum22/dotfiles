0fc3f10470b46a116f5d929ddbad1d90fa7150ab
Takafumi Asano <cariandrum22@gmail.com>
2020-08-18 13:09:53 +0900

    Organized fish configuration

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,113 +1,132 @@
 # Initialize
 thunnus.init
 
-# Basic configuration
+# Set basic environment variables
 set -x LC_ALL en_US.UTF-8
 set -x LANG en_US.UTF-8
-set -x PATH "/usr/local/sbin" $PATH
-set -x EDITOR (which emacs)
-set -x VISUAL (which code)
+set -x PATH "/usr/local/sbin" "$HOME/.local/bin" $PATH
 
-# Compiler and linker flag configuration
+if type -q emacs
+  set -x EDITOR (which emacs)
+end
+
+if type -q code
+  set -x VISUAL (which code)
+end
+
+if type -q gpg
+  set -x GPG_TTY (tty)
+  set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+  gpgconf --launch gpg-agent
+end
+
+# Load OneLogin's API Credential
+if [ -f "$HOME/.onelogin/credential.fish" ]
+  source "$HOME/.onelogin/credential.fish"
+end
+
+# Configure nix-shell
+if type -q bass
+  if [ -f "$HOME/.nix-profile/etc/profile.d/nix.sh" ]
+    bass source "$HOME/.nix-profile/etc/profile.d/nix.sh"
+  end
+end
+
+# Configuring the compiler and linker for macOS
 if [ (thunnus.os.platform.detect) = macOS ]
-  set -x CPPFLAGS "-I/usr/local/opt/llvm/include" "-I/usr/local/opt/openssl/include"
-  set -x LDFLAGS "-L/usr/local/opt/llvm/lib -Wl,-rpath,/usr/local/opt/llvm/lib" "-L/usr/local/opt/openssl/lib"
+  if [ -f "/usr/local/opt/llvm/bin" ]
+    set -x CPPFLAGS "-I/usr/local/opt/llvm/include" "-I/usr/local/opt/openssl/include"
+    set -x LDFLAGS "-L/usr/local/opt/llvm/lib -Wl,-rpath,/usr/local/opt/llvm/lib" "-L/usr/local/opt/openssl/lib"
+    set_path "/usr/local/opt/llvm/bin"
+  end
 end
 
-# Export default aws credentials
-if [ -f "$HOME/.aws/credentials" ]
-  thunnus.aws.credential_selector codetakt_cariandrum22 tf
+# Configure fzf
+if type -q fzf
+  set -U FZF_LEGACY_KEYBINDINGS 0
 end
 
-# fzf
-set -U FZF_LEGACY_KEYBINDINGS 0
+# Configure direnv
+if type -q direnv
+  eval (direnv hook fish)
+end
 
-# asdf-vm
+# Configure asdf-vm
 if [ -f "$HOME/.asdf/asdf.fish" ]
   source ~/.asdf/asdf.fish
 end
 
-# AWS
+# Configuration for aws cli and related tools
+## Export default aws credentials
+#if [ -f "$HOME/.aws/credentials" ]
+#  thunnus.aws.credential_selector saml tf
+#end
 set -x AWS_DEFAULT_REGION "ap-northeast-1"
 
-# Google Computing Platform
+# Configure the Google Cloud SDK
 ## The next line updates PATH for the Google Cloud SDK.
 if [ -f $HOME/Applications/Google/google-cloud-sdk/path.fish.inc ]
   source "$HOME/Applications/Google/google-cloud-sdk/path.fish.inc"
 end
-
 ## The next line enables shell command completion for gcloud.
 if [ -f $HOME/Applications/Google/google-cloud-sdk/completion.fish.inc ]
   source "$HOME/Applications/Google/google-cloud-sdk/completion.fish.inc"
 end
 
-## Initialize the *env such as rbenv, ndenv, etc.
-#arbenv "rbenv" "nodenv" "goenv" "pyenv" "plenv"
-
-# Go
-set -x GOENV_ROOT "$HOME/.goenv"
-set -x GOPATH "$HOME/Go"
-set_path "$GOENV_ROOT/bin"
-set_path "$GOPATH/bin"
-goenv init - | source
-
-# direnv
-eval (direnv hook fish)
-
-# pyenv-virtualenv
-pyenv virtualenv-init - | source
-
-# LLVM
-set_path "/usr/local/opt/llvm/bin"
-
-# Stack
-set_path "$HOME/.local/bin"
-
-# rustup
-set_path "$HOME/.cargo/bin"
-rustup completions fish | source
-
-# java
-if [ (thunnus.os.platform.detect) = macOS ]
-  set -x JAVA_HOME (/usr/libexec/java_home)
-else
-  set -x JAVA_HOME (type -p javac|xargs readlink -f|xargs dirname|xargs dirname)
+# Configure the Go Programming Language
+if type -q go
+  set -x GOENV_ROOT "$HOME/.goenv"
+  set -x GOPATH "$HOME/Go"
+  set_path "$GOENV_ROOT/bin"
+  set_path "$GOPATH/bin/"(uname -s | awk '{print tolower}')"_amd64"
 end
 
-# python
-set -x PYTHONSTARTUP "$HOME/.pythonstartup"
+# Configure rustup
+if type -q rustup
+  set_path "$HOME/.cargo/bin"
+  rustup completions fish | source
+end
 
-# Terraform
-set_path "$HOME/Applications/HashiCorp/Terraform/current/bin"
+# Configure Java
+if type -q java
+  if [ (thunnus.os.platform.detect) = macOS ]
+    set -x JAVA_HOME (/usr/libexec/java_home)
+  else
+    set -x JAVA_HOME (type -p javac|xargs readlink -f|xargs dirname|xargs dirname)
+  end
+end
 
-# Ansible
-set -x ANSIBLE_COW_SELECTION "random"
+# Configure Python
+if type -q python
+  set -x PYTHONSTARTUP "$HOME/.pythonstartup"
+end
 
-# Texinfo
-set_path "/usr/local/opt/texinfo/bin"
+# Configure nsible
+if type -q ansible
+  set -x ANSIBLE_COW_SELECTION "random"
+end
 
-# Nand2tetris Software Suite
-set_path "$HOME/Applications/nand2tetris/tools"
+# Configure Texinfo
+if [ (thunnus.os.platform.detect) = macOS ]
+  if [ -d "/usr/local/opt/texinfo/bin" ]
+    set_path "/usr/local/opt/texinfo/bin"
+  end
+end
 
-# CoreOS tools
-set_path "$HOME/Applications/CoreOS/bin"
-
-# alias
+# Configure aliases
 ## Generic
 alias where="command -v"
 alias j="jobs -l"
 alias ll="ls -lh"
 alias la="ls -lha"
 
-## Haskell
-#. $HOME/.nix-profile/etc/profile.d/nix.sh
-alias ghc="stack ghc --"
-alias ghci="stack ghci"
-alias runghc="stack runghc --"
-alias runhaskell="stack runghc --"
-
-# OPAM configuration
-source $HOME/.opam/opam-init/init.fish > /dev/null 2> /dev/null or true
+## for The Haskell Tool Stack
+if type -q stack
+  alias ghc="stack ghc --"
+  alias ghci="stack ghci"
+  alias runghc="stack runghc --"
+  alias runhaskell="stack runghc --"
+end
 
 # Normalize path(trim tail slash)
 thunnus.path.normalize $PATH
