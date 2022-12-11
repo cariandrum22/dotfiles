b7cb6649413094d72dc152dcae06cc422d5e22e9
Takafumi Asano <cariandrum22@gmail.com>
2022-12-11 21:39:42 +0900

    Remove shadow related settings from picom

--- config/nixpkgs/services/picom.nix
+++ config/nixpkgs/services/picom.nix
@@ -6,12 +6,6 @@
       0.06 # fade-in-step
       0.06 # fade-out-step
     ];
-    shadow = true;
-    shadowOffsets = [
-      (-4) # shadow-offset-x
-      (-4) # shadow-offset-y
-    ];
-    shadowOpacity = 0.9;
     inactiveOpacity = 0.8;
     opacityRules = [
       "100:class_g = 'Google-chrome' && focused"
@@ -24,15 +18,7 @@
       "100:class_g = 'Slack' && !focused"
     ];
     fadeExclude = [
-      # For using rofi
       "_NET_WM_NAME@:s = 'rofi'"
     ];
-    settings = {
-      # Anti flickerin
-      unredir-if-possible = false;
-
-      # shadows extra
-      clear-shadow   = true;
-    };
   };
 }
