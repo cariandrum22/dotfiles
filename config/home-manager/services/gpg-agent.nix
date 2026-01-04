7e9fd01cd5fba969b2e03d212d0e6a66ae847dae
Takafumi Asano <cariandrum22@gmail.com>
2026-01-05 01:35:51 +0900

    feat(gpg-agent): enable extra socket for agent forwarding
    Enable GPG agent extra socket to allow remote GPG agent forwarding
    over SSH. This allows using a locally-connected YubiKey or other
    smartcard to sign/encrypt on remote hosts.
    
    Usage: Configure SSH RemoteForward to forward the extra socket from
    local machine to remote host's agent socket.

--- config/home-manager/services/gpg-agent.nix
+++ config/home-manager/services/gpg-agent.nix
@@ -4,6 +4,7 @@
   services.gpg-agent = {
     enable = true;
     enableScDaemon = true;
+    enableExtraSocket = true;
     pinentry.package = if pkgs.stdenv.isDarwin then pkgs.pinentry-qt else pkgs.pinentry-gnome3;
   };
 }
