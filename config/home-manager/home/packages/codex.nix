1ec03e35ba98193caea3d4e8b09ef6a7f9c53ff9
Takafumi Asano <cariandrum22@gmail.com>
2026-02-15 09:14:06 +0900

    fix(codex-cli): use cargoPatches for vendoring

--- config/home-manager/home/packages/codex.nix
+++ config/home-manager/home/packages/codex.nix
@@ -122,10 +122,9 @@ pkgs.rustPlatform.buildRustPackage (
     sourceRoot = "source/codex-rs";
 
     cargoHash = cargoHashes.${pkgs.stdenv.system};
-    postUnpack = ''
-      cd "$sourceRoot"
-      patch -p1 < ${./remove-cargo-bin.patch}
-    '';
+    cargoPatches = [
+      ./remove-cargo-bin.patch
+    ];
 
     # Remove codex-utils-cargo-bin references from manifests/lockfile and
     # then stub any remaining callsites in code. Tests are disabled
