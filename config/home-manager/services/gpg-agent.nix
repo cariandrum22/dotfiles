93c6a920f2779ed61233662ce4f2f7d41cbd93c5
Takafumi Asano <cariandrum22@gmail.com>
2026-04-23 07:34:47 +0900

    fix(gpg-agent): enable headless linux profile

--- config/home-manager/services/gpg-agent.nix
+++ config/home-manager/services/gpg-agent.nix
@@ -1,10 +1,23 @@
-{ pkgs, ... }:
+args@{
+  pkgs,
+  ...
+}:
+
+let
+  isHeadless = args ? isHeadless && args.isHeadless;
+in
 
 {
   services.gpg-agent = {
     enable = true;
     enableScDaemon = true;
     enableExtraSocket = true;
-    pinentry.package = if pkgs.stdenv.isDarwin then pkgs.pinentry-qt else pkgs.pinentry-gnome3;
+    pinentry.package =
+      if pkgs.stdenv.isDarwin then
+        pkgs.pinentry-qt
+      else if isHeadless then
+        pkgs.pinentry-curses
+      else
+        pkgs.pinentry-gnome3;
   };
 }
