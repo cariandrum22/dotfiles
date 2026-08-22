5d2ba268a671695d31a4fe2aa810d8d2d766577f
cariandrum22 <1421516+cariandrum22@users.noreply.github.com>
2026-08-22 02:21:32 +0000

    chore(vscode): update VSCode Insiders

--- config/home-manager/programs/vscode/metadata.nix
+++ config/home-manager/programs/vscode/metadata.nix
@@ -1,13 +1,13 @@
 # This file is automatically updated by the update-vscode-insiders workflow
 rec {
   version = "1.135.0";
-  commit = "05bf4f5ce25799e2d0daf32bf90cb885ca75a0b9";
+  commit = "e1801a05353f5f3e557f524b63a9d09cf7d736d1";
   url = {
     aarch64-darwin = "https://vscode.download.prss.microsoft.com/dbazure/download/insider/${commit}/VSCode-darwin-arm64.zip";
-    x86_64-linux = "https://vscode.download.prss.microsoft.com/dbazure/download/insider/${commit}/code-insider-x64-1787203259.tar.gz";
+    x86_64-linux = "https://vscode.download.prss.microsoft.com/dbazure/download/insider/${commit}/code-insider-x64-1787332528.tar.gz";
   };
   sha256 = {
-    aarch64-darwin = "1ckip7vkdilchp959kcz6bfgr1lp2i456mmycyf1vgfmfzwaxamv";
-    x86_64-linux = "1vz8qsbav12mrkjfyy289qm5pgrrzil45wsd9fs5hc0vjv0j0160";
+    aarch64-darwin = "0xb6dnwmp8msdxa3p211b0cdslwdvgj2nnp21sf1650i19sy7z2m";
+    x86_64-linux = "0pvj2javql2dd8jzda6nqa4lf4wbj687rl2wlp2gmwll35i0vim0";
   };
 }
